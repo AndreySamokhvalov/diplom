ed3d1fec975669e6d33524fa65a773d075b929f9
Андрей Сергеевич Самохвалов <108537942+AndreySamokhvalov@users.noreply.github.com>
2024-01-31 20:20:30 +0200

    catalog dizaing

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -2,50 +2,86 @@
     <div class="catalog center">
         <h1>добро пожаловать в каталог</h1>
 
+        <div class="filter">
+            <div class="filter__title">
+                <h3>подобрать маршрут по:</h3>
+            </div>
+            <details class="filter__elem tit_complexity">
+                <summary class="filter__summary"><span class="filter__heading complex">СЛОЖНОСТЬ</span><svg
+                        class="filter__triangle" xmlns="http://www.w3.org/2000/svg" width="10" height="16"
+                        viewBox="0 0 10 16">
+                        <path
+                            d="M9.6747 7.29777C9.6627 7.28777 9.6467 7.28377 9.6347 7.27377L1.9207 0.291766C1.4867 -0.0982338 0.782701 -0.0982338 0.348701 0.291766C0.344701 0.295766 0.342702 0.299765 0.340702 0.303765C0.235613 0.390536 0.150739 0.499209 0.0920124 0.62219C0.0332859 0.74517 0.00212329 0.87949 0.000701904 1.01577V14.9818C0.00294433 15.1205 0.0356314 15.2571 0.0964546 15.3819C0.157278 15.5066 0.244754 15.6165 0.352702 15.7038L0.348701 15.7078C0.567073 15.8965 0.846069 16.0004 1.1347 16.0004C1.42333 16.0004 1.70233 15.8965 1.9207 15.7078L9.6747 8.70977C9.77682 8.62265 9.85883 8.51441 9.91506 8.39252C9.97129 8.27063 10.0004 8.138 10.0004 8.00377C10.0004 7.86953 9.97129 7.7369 9.91506 7.61501C9.85883 7.49312 9.77682 7.38489 9.6747 7.29777Z"
+                            fill="#C1C1C1" />
+                    </svg></summary>
 
-        <div class="catalog__filter">
-            <div class="catalog__filter_header">
-                <div class="catalog__filter_title">
-                    <h1 class="catalof__filter_title_elem filter__txt">подбать маршрут по:</h1>
+                <div class="filter__optional">
+                    <h3 :class="{ 'active': isActive13 }" v-on:click="isActive13 = !isActive13"
+                        class="filter__optional_name">#ЛЮБИТЕЛЬ</h3>
+                    <h3 :class="{ 'active': isActive23 }" v-on:click="isActive23 = !isActive23"
+                        class="filter__optional_name">#ТУРИСТ</h3>
+                    <h3 :class="{ 'active': isActive33 }" v-on:click="isActive33 = !isActive33"
+                        class="filter__optional_name">#ВЫЖИВШИЙ</h3>
+
+
                 </div>
-                <div class="catalog__filter_optional">
-                    <div class="catalog__filter_optional_list">
-                        <h5 class="catalog__filter_optional_list_elem filter__txt">рейтинг</h5>
-                        <!-- <img src="../assets/left_st.svg" alt="show me" class="show_or_not">  -->
-                    </div>
-                    <div class="catalog__filter_optional_list">
-                        <h5 class="catalog__filter_optional_list_elem filter__txt">сложность</h5>
-                        <button @click="showComplexity()"><img src="../assets/left_st.svg" alt="show me"
-                                class="show_or_not"></button>
-                    </div>
-                    <div class="catalog__filter_optional_list">
-                        <h5 class="catalog__filter_optional_list_elem filter__txt">длительность</h5>
-                        <button @click="showDuration()"><img src="../assets/left_st.svg" alt="show me"
-                                class="show_or_not"></button>
-                    </div>
-                    <div class="catalog__filter_optional_list">
-                        <h5 class="catalog__filter_optional_list_elem filter__txt">размер группы</h5>
-                        <button @click="showSize()"><img src="../assets/left_st.svg" alt="show me"
-                                class="show_or_not"></button>
-                    </div>
+
+            </details>
+
+            <details class="filter__elem tit_time">
+                <summary class="filter__summary"><span class="filter__heading long_time">ДЛИТЕЛЬНОСТЬ</span><svg
+                        class="filter__triangle" xmlns="http://www.w3.org/2000/svg" width="10" height="16"
+                        viewBox="0 0 10 16">
+                        <path
+                            d="M9.6747 7.29777C9.6627 7.28777 9.6467 7.28377 9.6347 7.27377L1.9207 0.291766C1.4867 -0.0982338 0.782701 -0.0982338 0.348701 0.291766C0.344701 0.295766 0.342702 0.299765 0.340702 0.303765C0.235613 0.390536 0.150739 0.499209 0.0920124 0.62219C0.0332859 0.74517 0.00212329 0.87949 0.000701904 1.01577V14.9818C0.00294433 15.1205 0.0356314 15.2571 0.0964546 15.3819C0.157278 15.5066 0.244754 15.6165 0.352702 15.7038L0.348701 15.7078C0.567073 15.8965 0.846069 16.0004 1.1347 16.0004C1.42333 16.0004 1.70233 15.8965 1.9207 15.7078L9.6747 8.70977C9.77682 8.62265 9.85883 8.51441 9.91506 8.39252C9.97129 8.27063 10.0004 8.138 10.0004 8.00377C10.0004 7.86953 9.97129 7.7369 9.91506 7.61501C9.85883 7.49312 9.77682 7.38489 9.6747 7.29777Z"
+                            fill="#C1C1C1" />
+                    </svg></summary>
+
+                <div class="filter__optional">
+                    <h3 :class="{ 'active': isActive21 }" v-on:click="isActive21 = !isActive21"
+                        class="filter__optional_name">1 - 2 дня</h3>
+                    <h3 :class="{ 'active': isActive22 }" v-on:click="isActive22 = !isActive22"
+                        class="filter__optional_name">3 - 5 дней</h3>
+                    <h3 :class="{ 'active': isActive23 }" v-on:click="isActive23 = !isActive23"
+                        class="filter__optional_name">7 + дней</h3>
+
+
+
                 </div>
-            </div>
-            <div v-show=complexityVisible class="filter__buttons_complexity hide__btn_block">
-                <button class="filter__buttons_complexity_first hide__btn">легко</button>
-                <button class="filter__buttons_complexity_second hide__btn ">нормально</button>
-                <button class="filter__buttons_complexity_third hide__btn ">сложно</button>
-            </div>
-            <div v-show=durationVisible class="filter__buttons_duration hide__btn_block">
-                <button class="filter__buttons_duration_first hide__btn">1-2 дня</button>
-                <button class="filter__buttons_duration_second hide__btn ">3-5 дней</button>
-                <button class="filter__buttons_duration_third hide__btn ">7+ дней</button>
-            </div>
-            <div v-show=sizeVisible class="filter__buttons_size hide__btn_block">
-                <button class="filter__buttons_size_first hide__btn">до 6 человек</button>
-                <button class="filter__buttons_size_second hide__btn ">7-15 человек</button>
-                <button class="filter__buttons_size_third hide__btn ">более 15 человек</button>
+
+            </details>
+
+            <details class="filter__elem tit_size">
+                <summary class="filter__summary"><span class="filter__heading capas">РАЗМЕР ГРУППЫ</span><svg
+                        class="filter__triangle" xmlns="http://www.w3.org/2000/svg" width="10" height="16"
+                        viewBox="0 0 10 16">
+                        <path
+                            d="M9.6747 7.29777C9.6627 7.28777 9.6467 7.28377 9.6347 7.27377L1.9207 0.291766C1.4867 -0.0982338 0.782701 -0.0982338 0.348701 0.291766C0.344701 0.295766 0.342702 0.299765 0.340702 0.303765C0.235613 0.390536 0.150739 0.499209 0.0920124 0.62219C0.0332859 0.74517 0.00212329 0.87949 0.000701904 1.01577V14.9818C0.00294433 15.1205 0.0356314 15.2571 0.0964546 15.3819C0.157278 15.5066 0.244754 15.6165 0.352702 15.7038L0.348701 15.7078C0.567073 15.8965 0.846069 16.0004 1.1347 16.0004C1.42333 16.0004 1.70233 15.8965 1.9207 15.7078L9.6747 8.70977C9.77682 8.62265 9.85883 8.51441 9.91506 8.39252C9.97129 8.27063 10.0004 8.138 10.0004 8.00377C10.0004 7.86953 9.97129 7.7369 9.91506 7.61501C9.85883 7.49312 9.77682 7.38489 9.6747 7.29777Z"
+                            fill="#C1C1C1" />
+                    </svg></summary>
+
+                <div class="filter__optional">
+                    <h3 :class="{ 'active': isActive31 }" v-on:click="isActive31 = !isActive31"
+                        class="filter__optional_name">до 6 человек</h3>
+                    <h3 :class="{ 'active': isActive32 }" v-on:click="isActive32 = !isActive32"
+                        class="filter__optional_name">6 - 15 человек</h3>
+                    <h3 :class="{ 'active': isActive33 }" v-on:click="isActive33 = !isActive33"
+                        class="filter__optional_name">15 + человек</h3>
+
+
+                </div>
+
+            </details>
+            <div class="no_filter">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="43" viewBox="0 0 50 43">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M37.5 19.1111C40.8152 19.1111 43.9946 20.3695 46.3388 22.6096C48.683 24.8496 50 27.8877 50 31.0556C50 37.6523 44.4036 43 37.5 43C30.5964 43 25 37.6523 25 31.0556C25 24.4588 30.5964 19.1111 37.5 19.1111ZM37.5 21.5C31.9772 21.5 27.5 25.7782 27.5 31.0556C27.5 36.3329 31.9772 40.6111 37.5 40.6111C43.0228 40.6111 47.5 36.3329 47.5 31.0556C47.5 25.7782 43.0228 21.5 37.5 21.5ZM41.7824 25.2743L43.5502 26.9635L39.2676 31.0555L43.5502 35.1476L41.7824 36.8368L37.5 32.7446L33.2176 36.8368L31.4498 35.1476L35.7322 31.0555L31.4498 26.9635L33.2176 25.2743L37.5 29.3663L41.7824 25.2743ZM40 0L22.5 20.3801V34.0417H17.5L17.5 20.3802L0 0H40Z"
+                        fill="#989898" />
+                </svg>
             </div>
         </div>
+
+
 
 
 
@@ -67,24 +103,24 @@
                     <star-rating :rating=card.rating :read-only="true" :increment="0.01"></star-rating>
                 </div>
                 <!-- настройка вместительности и сложности -->
-                
-                    <div class="item_capacity">
-                        <span class="item_capacity_text">
-                            вместимость группы:
-                        </span>
-                        <span class="item_capacity_int item_capacity_text">
-                            {{ card.capacity }}
-                        </span>
-                    </div>
-                    <div class="item_complexity">
-                        <span class="item_complexity_text">
-                            сложность:
-                        </span>
-                        <span class="item_complexity_int item_complexity_text ">
-                            {{ card.lvl }}
-                        </span>
-                    </div>
-                
+
+                <div class="item_capacity">
+                    <span class="item_capacity_text">
+                        вместимость группы:
+                    </span>
+                    <span class="item_capacity_int item_capacity_text">
+                        {{ card.capacity }}
+                    </span>
+                </div>
+                <div class="item_complexity">
+                    <span class="item_complexity_text">
+                        сложность:
+                    </span>
+                    <span class="item_complexity_int item_complexity_text ">
+                        {{ card.lvl }}
+                    </span>
+                </div>
+
 
             </div>
 
@@ -109,11 +145,20 @@ export default {
 
     data() {
         return {
+            isActive11: false,
+            isActive12: false,
+            isActive13: false,
+            isActive21: false,
+            isActive22: false,
+            isActive23: false,
+            isActive31: false,
+            isActive32: false,
+            isActive33: false,
             rating: "No Rating Selected",
             currentRating: "No Rating",
             currentSelectedRating: "No Current Rating",
             boundRating: 3,
-            // pathList1: [],
+
             complexityVisible: false,
             durationVisible: false,
             sizeVisible: false,
@@ -138,7 +183,10 @@ export default {
     },
 
     methods: {
-
+        addClass() {
+            this.isActive = !this.isActive;
+            return this.isActive
+        },
         setRating: function (rating) {
             this.rating = "You have Selected: " + rating + " stars";
         },
@@ -176,6 +224,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.active {
+    color: $btn_color
+}
+
 .item_img {
     width: 390px;
     height: 626px;
@@ -199,7 +251,7 @@ export default {
     position: relative;
     top: -273px;
     left: 80px;
-    
+
 }
 
 .path__card_name_name {
@@ -221,8 +273,8 @@ export default {
     position: relative;
     top: -200px;
     left: 48px;
-    transition:all .3s ;
-    
+    transition: all .3s;
+
 }
 
 .item_capacity {
@@ -234,7 +286,7 @@ export default {
     flex-direction: row;
     flex-wrap: nowrap;
     justify-content: space-between;
-    transition:all .3s ;
+    transition: all .3s;
 
     &_text {
         color: rgb(0, 0, 0);
@@ -255,7 +307,7 @@ export default {
     justify-content: space-between;
     top: -100px;
     left: 18px;
-    transition:all .3s ;
+    transition: all .3s;
 
 
     &_text {
@@ -309,70 +361,175 @@ export default {
     margin-right: 20px;
 }
 
-.c_and_c{
-    width: 390px;
-    height: auto;
-    // border-color: #000;
-    border: 3px solid #000;
-}
+.filter {
 
-.catalog__filter {
-    max-width: $widthSite;
-    height: auto;
-    background-color: #b8b8b8;
-    margin-bottom: 34px;
     display: flex;
+    flex-direction: row;
+    width: 1389px;
+    height: 102px;
+    flex-shrink: 0;
+    border-radius: 60px;
+    border: 1px solid var(--btn_bg, #000);
+    position: relative;
     align-items: center;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    justify-content: center;
+    justify-content: space-between;
 
-    &_title {
-        color: $textColorContrast;
+    &__title {
+        color: #989898;
         font-family: Lack;
-        font-size: 14px;
+        font-size: 22px;
         font-style: normal;
         font-weight: 400;
         line-height: normal;
         text-transform: uppercase;
-        margin-left: 25px;
+        padding-left: 45px;
+    }
+
+
+
+
+
+    &__summary {
+        display: flex;
+        align-items: center;
+        gap: 12px;
+    }
+
+    &__elem {
+        width: 190px;
+        height: 240px;
+        box-sizing: border-box;
+        position: absolute;
+        top: 40px;
+        left: 100px;
+
+
+
+
+        &[open] {
+            background-color: #fff;
+            box-shadow: 6px 4px 35px rgba(0, 0, 0, 0.30);
+            border-top: 0px;
+            // transform: transition all .3s;
+            // z-index: -2;
+
+        }
     }
 }
 
-.catalog__filter_header {
-    display: flex;
-    flex-direction: row;
+.no_filter {
+    margin-right: 50px;
+    margin-top: 16px;
+
+
+    &:hover path {
+        fill: $btn_color;
+    }
+    &:active path{
+        fill: rgb(234, 119, 119);
+        border: 1px solid var(--btn_bg, #b30000);
+
+    }
+
 }
 
-.catalog__filter_optional {
+.complex {
     display: flex;
-    gap: 20px;
+    margin-left: 28px;
+    padding-top: 10px;
 }
 
-.catalog__filter_optional_list {
+.long_time {
     display: flex;
-    flex-direction: row;
+    margin-left: 20px;
+    padding-top: 10px;
+}
+
+.capas {
+    display: flex;
+    margin-left: 16px;
+    padding-top: 10px;
+
+}
+
+.tit_complexity {
+    margin-left: 350px;
+}
+
+.tit_time {
+    //    position: absolute;
+    margin-left: 650px;
+
+}
+
+.tit_size {
+
+    margin-left: 950px;
+}
+
+.filter__heading {
+    position: relative;
+    top: -4px;
+
+
+}
+
+//  настройка опций фильтра
+.filter__optional {
+    display: flex;
+    flex-direction: column;
     flex-wrap: nowrap;
-    align-items: center;
-    gap: 12px;
+    align-items: flex-start;
+
+
+    &_name {
+        margin-left: 12px;
+        margin-top: 30px;
+        font-family: Lack;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        text-transform: uppercase;
+
+    }
 }
 
-.catalog__filter_optional_list_elem {
-    color: $textColorContrast;
-    font-family: Lack;
-    font-size: 28px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-    text-transform: uppercase;
-    margin-left: 25px;
+
+
+.filter__elem[open] .filter__heading {
+    color: $btn_color;
 }
 
-.show_or_not {
-    width: 20px;
-    background-color: #b8b8b8;
-    border: 0px;
+.filter__elem[open] path {
+    transform: transition all .3s;
+    fill: $btn_color;
 }
+
+.filter__triangle {
+    transition: all .3s;
+}
+
+.filter__elem[open] .filter__triangle {
+    rotate: (90deg);
+    transform: transition all .3s;
+}
+
+//  не актив, другой псевдокласс
+.filter__optional_name:hover {
+    color: $btn_color;
+
+}
+
+
+
+
+
+
+
+
+
+
 
 .hide__btn {
     color: $textColorLack;
@@ -403,7 +560,7 @@ export default {
     transition: width 2s;
 }
 
-.catalog-preview__catalog_item{
+.catalog-preview__catalog_item {
     border: 3px solid #000;
     border-top: 0;
     border-left: 0;
@@ -412,12 +569,13 @@ export default {
     margin-bottom: 70px;
     margin-top: 170px;
     border-radius: 20px;
-    transition:all .3s ;
+    transition: all .3s;
     z-index: 5;
     // transform: transition all .3s ;
-    
+
 }
-.catalog-preview__catalog_item:hover{
+
+.catalog-preview__catalog_item:hover {
     border: 3px solid $btn_color;
     // border-top: 0;
     // border-left: 0;
@@ -430,28 +588,30 @@ export default {
     width: 464px;
     position: relative;
     z-index: 10;
-    transform: transition border .3s ;
+    transform: transition border .3s;
     margin-left: -24px;
     margin-right: -24px;
 }
+
 .catalog-preview__catalog_item:hover .path__card_name_img {
     position: relative;
     left: 0;
     transition-duration: .3s;
     // width: 486px;
-   
-}
-img{
-    transition:  .3s;
+
 }
 
-.catalog-preview__catalog_item:hover .path__card_name_name{
+img {
+    transition: .3s;
+}
+
+.catalog-preview__catalog_item:hover .path__card_name_name {
     top: -230px;
     left: 48px;
     color: $textColorContrast;
-  
+
     font-family: Lack;
-    
+
     font-style: normal;
     font-weight: 400;
     line-height: normal;
@@ -468,14 +628,15 @@ img{
     // left: 160px;
     transition-duration: .3s;
 }
-.catalog-preview__catalog_item:hover .item_capacity{
+
+.catalog-preview__catalog_item:hover .item_capacity {
     width: 418px;
     transition-duration: .3s;
- 
- }
- .catalog-preview__catalog_item:hover .item_complexity{
+
+}
+
+.catalog-preview__catalog_item:hover .item_complexity {
     width: 418px;
     transition-duration: .3s;
-    
- }
-</style>
+
+}</style>
